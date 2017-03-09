f6f432352da53f21f149c78fd4a1781be7dec7d8
aDrz <adrien.nouvellet@gmail.com>
2017-03-09 11:12:44 +0100

    dfd

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#from distutils.core import setup
 from setuptools import setup, find_packages
 
-setup(name='py-mmm',
+setup(name='pymmm',
       version='0.1',
       description='Implementation of EM for mixture markov model',
       url='http://github.com/adrz/py-mmm',
       author='Adrien N.',
       author_email='adrien.nouvellet@gmail.com',
       license='MIT',
-      packages=find_packages(exclude=('tests', 'tests.*')),
+      packages = find_packages(),
       install_requires=["pandas","numpy"],
       zip_safe=False)
